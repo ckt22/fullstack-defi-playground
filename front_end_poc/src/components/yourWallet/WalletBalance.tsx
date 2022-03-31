830aa573ed002d60628a17bb16b7cbd051ee8188
ckt22 <tangchekkit@kidpairing.com>
2022-03-31 16:33:40 +0800

    remove console logs

--- front_end_poc/src/components/yourWallet/WalletBalance.tsx
+++ front_end_poc/src/components/yourWallet/WalletBalance.tsx
@@ -14,15 +14,8 @@ export const WalletBalance = ({ token }: WalletBalanceProps) => {
     const { account } = useEthers();
     const tokenBalance = useTokenBalance(address, account); // in wei
     const etherBalance = useEtherBalance(account);
-    // const [{ data, error, loading }, getBalance] = useBalance({
-    //     addressOrName: '0x63Ed5a2f1f7dfbe87449379e520A88585C1e233a',
-    //   })
     
-    console.log(etherBalance);
-    console.log(tokenBalance);
     const formattedTokenBalance: number = tokenBalance ? parseFloat(formatUnits(tokenBalance, 18)) : 0;
-    // if (loading) return <div>Fetching balance…</div>
-    // if (error) return <div>Error fetching balance</div>
     return (<BalanceMsg
         label={`Your un-staked ${name} balance`}
         tokenImgSrc={image}
